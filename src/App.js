695a0cd7abd2ce19986772844e25c00b4dd8e6ad
Sparsh <97156719+Sparsh-pixel@users.noreply.github.com>
2023-09-13 19:18:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Contacts from "./Components/Contacts";
+
+
+function App() {
+  return (
+    <>
+     
+     <Contacts/> 
+      
+    </>
+  );
+}
+
+export default App;
